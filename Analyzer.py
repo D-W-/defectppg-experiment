57ebd619bcbb75861c4e66daba41cc5797e157bd
harry <phoenix.seek@gmail.com>
2018-02-13 15:05:21 +0800

    analyze projects

--- Analyzer.py
+++ Analyzer.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python
 # coding=utf8
 
-from subprocess import call
+from subprocess import call, check_output
+from os import path
 
 """
 Created by Han Wang at 2/10/18. 
@@ -9,32 +10,34 @@ Created by Han Wang at 2/10/18.
 
 __author__ = 'Han Wang'
 
-ANALYZER = "scan_build_3.9"
+ANALYZER = "scan-build-3.9 "
+
+LOCATION = "/home/harry/Downloads"
 
 
 class Analyzer(object):
+	def __init__(self, location):
+		self.location = location
+		tars = check_output("ls " + location + " | grep \".*.tar.gz\"", shell=True)
+		self.tars = tars.strip().split("\n")
+		self.projects = [path.join(location, tar.split(".tar.gz")[0]) for tar in self.tars]
 
-    def __init__(self, location):
-        self.location = location
+	def unzip(self):
+		for tar in self.tars:
+			call("cd " + self.location + " tar -zxvf " + tar, shell=True)
 
-    def analyse(self):
-        call([ANALYZER, self.location, "-o output"])
-        print("analyze phase done.")
-
-    @staticmethod
-    def test():
-	    # a = call(["cd", "output"])
-	    # print(call("ls"))
-	    call("cd output && ls", shell=True)
-	    # call("ls")
-        # print(call(["gcc", "-c "]))
-        # print("hello")
+	def analyse(self):
+		for tar in self.projects:
+			call("cd " + tar + " && ./config no-asm", shell=True)
+			call("cd " + tar + " && " + ANALYZER + tar + " make -o output" , shell=True)
+		print("analyze phase done.")
 
 
 def main():
-    a = Analyzer("pwd")
-    a.test()
+	a = Analyzer(LOCATION)
+	a.unzip()
+	# a.analyse()
 
 
 if __name__ == '__main__':
-    main()
+	main()
