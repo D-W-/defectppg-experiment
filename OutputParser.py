f09e3eb05eb0d898628a9f5cc11f7e8dda779abb
harry <phoenix.seek@gmail.com>
2018-02-14 17:39:55 +0800

    paser output in batch

--- OutputParser.py
+++ OutputParser.py
@@ -7,6 +7,7 @@ Created by Han Wang at 2/11/18.
 
 from bs4 import BeautifulSoup
 import os
+from subprocess import check_output
 
 __author__ = 'Han Wang'
 
@@ -18,29 +19,30 @@ def toText(item):
 	return item.text
 
 class OutputParser:
-	def __init__(self, location):
-		# type: (object) -> object
-		self.location = location
-		with open(os.path.join(location, INPUT)) as f:
-			self.content = f.read()
-		# print self.content
+	def __init__(self):
+		pass
 
-	def parse(self):
+	def parse(self, location):
+		with open(os.path.join(location, INPUT)) as f:
+			content = f.read()
 		# find table contents in html
-		soup = BeautifulSoup(self.content, 'html.parser')
+		soup = BeautifulSoup(content, 'html.parser')
 		table = soup.findAll('tbody')[0]
 		data = [map(toText, tr.findAll('td')) for tr in table.findAll('tr')]
 		data = ["::".join(line[0:5]) + '\n' for line in data]
 		# store parsed table data into file
-		with open(os.path.join(self.location, OUTPUT), 'w') as f:
+		with open(os.path.join(location, OUTPUT), 'w') as f:
 			f.writelines(data)
 
-
-		
 		
 def main():
-	parser = OutputParser("output")
-	parser.parse()
+	location = "output"
+	out_folders = check_output("ls " + location + " | grep \"scan\"", shell=True)
+	out_folders = out_folders.strip().split("\n")
+
+	parser = OutputParser()
+	for folder in out_folders:
+		parser.parse(os.path.join(location, folder))
 	
 
 if __name__ == '__main__':
