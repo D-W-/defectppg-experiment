7dd9898e9bec6e0ce647c49303862ef0e1c90ab2
harry <phoenix.seek@gmail.com>
2018-02-28 22:36:50 +0800

    lable defects and change relative path to absolute path

--- DefectLabeler.py
+++ DefectLabeler.py
@@ -8,7 +8,9 @@ Created by Han Wang at 2/12/18.
 
 __author__ = 'Han Wang'
 
-from subprocess import check_output
+# FOLDER = "/home/harry/Downloads/task/"
+
+from subprocess import check_output, call
 from os.path import join
 import Analyzer
 
@@ -53,7 +55,7 @@ class DefectLabeler:
         return [Defect(*defect.strip().split("::")) for defect in defects]
 
     # label previous defects lists according to current defect list
-    def label(self, prev_location, cur_location, out_location):
+    def label(self, prev_location, cur_location, out_location, defect_location):
         cur_defects = DefectLabeler.defects_getter(cur_location)
         cur_counter = Counter()
         for defect in cur_defects:
@@ -65,6 +67,10 @@ class DefectLabeler:
                 cur_counter[defect] -= 1
                 defect.unfix()
 
+        # defect to absolute path
+        for defect in prev_defects :
+            defect.location = join(defect_location, defect.location)
+
         with open(out_location, "w") as f:
             f.writelines([str(defect) for defect in prev_defects])
 
@@ -73,13 +79,26 @@ def main():
     location = Analyzer.LOCATION
     d = "defects.list"
     # out_folders = check_output("ls " + loc + " | grep \"scan\"", shell=True)
+    projects_folders = check_output("cd " + location + " && ls -d */", shell=True)
+    projects_folders = [join(location, project) for project in projects_folders.strip().split("\n")]
     out_folders = check_output("cd " + location + "&& ls -d */output/*/", shell=True)
     out_folders = [join(location, folder) for folder in out_folders.strip().split("\n")]
     labeler = DefectLabeler()
-    for p, c in zip(out_folders[:-1], out_folders[1:]):
-        print(p, c)
-        labeler.label(join(p, d), join(c, d), join(p, "out.list"))
+    for p, c, fd in zip(out_folders[:-1], out_folders[1:], projects_folders):
+        print(p, c, fd)
+        labeler.label(join(p, d), join(c, d), join(p, "out.list"), fd)
+
+        break
+
+
+# def test():
+#     with open("/home/harry/Downloads/openssl-1.0.0a/output/2018-02-27-212749-38912-1/out.list") as f:
+#         defects = f.readlines()
+#     defects = [Defect(*defect.strip().split("::")) for defect in defects]
+#     for defect in defects:
+#         call("cp " + defect.location + ".ll /home/harry/Downloads/task1", shell=True)
 
 
 if __name__ == "__main__":
     main()
+    # test()
